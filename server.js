0c25ed2431f60115f346b53845bf1b6dd879552d
ivorysmoker <deriaz.yannick@bluewin.ch>
2015-01-17 19:48:34 +0100

    Upload

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ app.get('/JumpGo', function (req, res) {
      res.sendFile(__dirname + '/views/index.html');
 });
  
-var PhysikNumber = 0;			
+var PhysikNumber = 0;	
+var PlayerCollection = 0;		
 io.sockets.on('connection', function (socket) {
 var clientIp = socket.request.connection.remoteAddress; // Liefer die IP
 var Vorhanden = BenutzerIp.indexOf(clientIp);
@@ -39,7 +40,7 @@ OnlineUsers[socket.nickname] = socket;
 console.log(OnlineUsers[socket.nickname]);
 //Setze den Player Start Punkt
 socket.XCoords = 15;
-socket.YCoords = 15;
+socket.YCoords = 380;
 socket.PlayerOrder = BenutzerIpName.length;
 io.sockets.emit("PlayerSpawn", socket.XCoords, socket.YCoords, BenutzerIpName.length);
 //updateNicknamesOnline();
@@ -57,7 +58,7 @@ io.sockets.emit("PlayerSpawn", socket.XCoords, socket.YCoords, BenutzerIpName.le
 		BenutzerIp.push(clientIp);
 		BenutzerIpName.push(socket.nickname);
 		socket.XCoords = 15;
-		socket.YCoords = 15;
+		socket.YCoords = 380;
 		socket.PlayerOrder = BenutzerIpName.length;
 		socket.emit("Weiterleitung");
        }
@@ -69,27 +70,33 @@ io.sockets.emit("PlayerSpawn", socket.XCoords, socket.YCoords, BenutzerIpName.le
 		if(Direction === "Left"){
 			console.log(socket.nickname+socket.PlayerOrder);
 			if(socket.XCoords > 0){
+				for(x=0; x < 2; x++){
 				socket.XCoords = socket.XCoords - 1; // Geschwindigkeit Left
 				io.sockets.emit('PlayerMovment', socket.XCoords, socket.YCoords, socket.PlayerOrder, "Left");
 				console.log(socket.nickname+" PlayerMove Position: X: "+socket.XCoords+" Y:"+socket.YCoords);
+				}
 			}
 		}else if(Direction === "Down"){
-			if(socket.YCoords > 0 && socket.YCoords < 500){
+			if(socket.YCoords > 0 && socket.YCoords < 400){
 				socket.YCoords = socket.YCoords + 1; // Geschwindigkeit Down
 				io.sockets.emit('PlayerMovment', socket.XCoords, socket.YCoords, socket.PlayerOrder, "Down");
 				console.log(socket.nickname+" PlayerMove Position: X: "+socket.XCoords+" Y:"+socket.YCoords);
 			}
 		}else if(Direction === "Right"){
 			if(socket.XCoords > 0 && socket.XCoords < 500){
+				for(x=0; x < 2; x++){
 				socket.XCoords = socket.XCoords + 1; // Geschwindigkeit Right
 				io.sockets.emit('PlayerMovment', socket.XCoords, socket.YCoords, socket.PlayerOrder, "Right");
 				console.log(socket.nickname+" PlayerMove Position: X: "+socket.XCoords+" Y:"+socket.YCoords);
+				}
 			}
 		}else if(Direction === "Jump"){
-			if(socket.YCoords > 0 && socket.YCoords < 500){
+			if(socket.YCoords > 350 && socket.YCoords < 500){
+				for(x=0; x < 50; x++){
 				socket.YCoords = socket.YCoords - 1; // Geschwindigkeit Jump
 				io.sockets.emit('PlayerMovment', socket.XCoords, socket.YCoords, socket.PlayerOrder, "Jump");
 				console.log(socket.nickname+" PlayerMove Position: X: "+socket.XCoords+" Y:"+socket.YCoords);
+				}
 			}
 		}
 	});	
@@ -101,14 +108,27 @@ io.sockets.emit("PlayerSpawn", socket.XCoords, socket.YCoords, BenutzerIpName.le
 					clearTimeout(myVar);
 				}
 				myVar = setTimeout(function () {  
-					console.log("Physik Trigger");
-					PhysikNumber++;    
-						socket.emit("PlayerMovment");
+					//console.log("Physik Trigger");
+					PhysikNumber++;
+					if(typeof BenutzerIpName[0] !== 'undefined'){
+						if(OnlineUsers[BenutzerIpName[0]].YCoords < 400){
+						//console.log(OnlineUsers[BenutzerIpName[0]].YCoords);
+							OnlineUsers[BenutzerIpName[0]].YCoords++;	
+							io.sockets.emit("PlayerMovment", OnlineUsers[BenutzerIpName[0]].XCoords, OnlineUsers[BenutzerIpName[0]].YCoords, 1 ,"Down");
+						}
+					}
+					if(typeof BenutzerIpName[1] !== 'undefined'){
+						if(OnlineUsers[BenutzerIpName[1]].YCoords < 400){
+						OnlineUsers[BenutzerIpName[1]].YCoords++;	
+						io.sockets.emit("PlayerMovment", OnlineUsers[BenutzerIpName[1]].XCoords, OnlineUsers[BenutzerIpName[1]].YCoords, 2 ,"Down");
+						}
+					}
+					//io.sockets.emit("PlayerMovment", socket.XCoords, socket.YCoords , 1, "Down");
 					if (PhysikNumber > 0) {            
 						PhsyikTrigger();             
 					}                   
 					
-				}, 10000) //1000.0/30.0 30 fps +-
+				}, 1000.0/30.0) //1000.0/30.0 30 fps +-
 			}
 	PhsyikTrigger(); 		
 });	
